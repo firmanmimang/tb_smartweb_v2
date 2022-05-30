59b06815575c66a696a6d00ca0fb054fa134670f
firmanmimang <fhidayat131@gmail.com>
2022-05-31 03:10:06 +0700

    comment weather widget layout

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -45,7 +45,7 @@
     @include('frontend.partials.scroll-top')
 
     {{-- floating weather info --}}
-    @include('frontend.partials.weather')
+    {{-- @include('frontend.partials.weather') --}}
 
     <!-- Optional Js, popper.js first then bootstrap JS-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
